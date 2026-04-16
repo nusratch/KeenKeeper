03d2991cbecf2e5aa26db3633774f614a96557ea
nusrat <nusratchy.002@gmail.com>
2026-04-17 04:54:49 +0600

    notfound page done

--- src/assets/component/notfound/Notfound.jsx
+++ src/assets/component/notfound/Notfound.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
+    
     <div className="flex flex-col items-center justify-center h-[60vh] text-center">
       <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
       <p className="text-lg text-gray-600 mb-6">
